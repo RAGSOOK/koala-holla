35dc779691877ca42efe1b41cbab194f96ca065f
Waldyn Benbenek <wben1201@gmail.com>
2019-01-10 11:35:33 -0600

    this is the GET I hope.

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pg = require('pg');
 
+const Pool = pg.Pool;
 // DB CONNECTION
-
+const pool = new Pool ({
+    database: 'koalas',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 10000
+});
 
 // GET
+router.get('/', (req, res) => {
+    let queryText = `SELECT * FROM "koalas" ORDER BY "rating" DESC LIMIT 100;`;
+    pool.query(queryText).then((result) => {
+        //console.log(result);
+        res.send(result.rows);
+    }).catch((error) => {
+      console.log(`error in get koalas ${error}`) 
+      res.send() 
+    });
+});
 
 
 // POST
