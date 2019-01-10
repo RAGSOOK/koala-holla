40a54365bf0e96091a0258ba3301d79da6f0d866
Tang Yang <ty4081097@gmail.com>
2019-01-10 14:18:23 -0600

    modify code.

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -24,12 +24,8 @@ koalaRouter.get('/', (req, res) => {
     });
 });
 koalaRouter.put('/:id', (req, res) => {
-<<<<<<< HEAD
-    const queryText = `UPDATE "koalas" SET "ready_to_transfer" = 'True' WHERE "id" = $1;`
-=======
     const queryText = `UPDATE "koalas" SET "ready_to_transfer" = 'true' 
     WHERE "id" = $1;`
->>>>>>> 254efdd4e9e810fb014bc2043316ba74679ecb61
     pool.query(queryText, [req.params.id]).then((result) => {
         res.sendStatus(200);
 });
